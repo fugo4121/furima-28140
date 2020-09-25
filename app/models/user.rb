10424fc5106436b59e3683aea7e6ddaa95aaf035
fugo4121 <lancerevo345@yahoo.co.jp>
2020-09-26 02:14:51 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+         validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: 'は半角英数字混合にしてください' }
+         validates :nickname, presence: true
+         validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'は全角（漢字・ひらがな・カタカナ）を入力してください' }
+         validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'は全角（漢字・ひらがな・カタカナ）を入力してください' }
+         validates :first_name_kana, presence: true, format: { with: /\A[ァ-ン]+\z/, message: 'は全角（カタカナ）を入力してください' }
+         validates :last_name_kana, presence: true, format: { with: /\A[ァ-ン]+\z/, message: 'は全角（カタカナ）を入力してください' }
+         validates :birthday, presence: true
+         
 end
